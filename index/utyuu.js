22f90c055475d106fd83815a03227de4379804fa
true525 <104573302+true525@users.noreply.github.com>
2024-04-30 14:05:42 +0900

    Create utyuu.js

---
+++ index/utyuu.js
@@ -0,0 +1,6 @@
+const exploreButton = document.querySelector('.hero-content button');
+
+exploreButton.addEventListener('click', () => {
+  // Add your desired action here
+  console.log('Learn More button clicked');
+});
