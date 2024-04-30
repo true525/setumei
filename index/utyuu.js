4ad6e23cca85efe5432a7b0e356914e4c1b43acc
true525 <104573302+true525@users.noreply.github.com>
2024-04-30 14:09:00 +0900

    Delete index directory

--- index/utyuu.js
+++
@@ -1,6 +0,0 @@
-const exploreButton = document.querySelector('.hero-content button');
-
-exploreButton.addEventListener('click', () => {
-  // Add your desired action here
-  console.log('Learn More button clicked');
-});
